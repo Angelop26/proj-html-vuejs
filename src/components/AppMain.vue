2b6060b407af424e2d62a4d3075f03b6f9e48e50
Angelop26 <angeloprospero26@gmail.com>
2023-04-26 20:11:04 +0200

    cta complete

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,15 +1,18 @@
 <script>
 import MainJumboCarousel from './MainComponents/MainJumboCarousel.vue';
+import MainCta from './MainComponents/MainCta.vue';
 export default{
     name:'AppMain',
     components:{
-        MainJumboCarousel
+        MainJumboCarousel,
+        MainCta
     }
 }
 </script>
 
 <template>
 <MainJumboCarousel />
+<MainCta :text="`How to Enroll Your Child to a Class?`" :ButtonText="`LEARN MORE`"/>
 </template>
 
 <style lang="scss" scoped>
---
+++ src/components/MainComponents/MainCta.vue
@@ -0,0 +1,47 @@
+<script>
+export default{
+    name:'MainCta',
+    props:{
+        text: String,
+        ButtonText: String
+    }
+}
+</script>
+
+<template>
+    <section>
+        <div class="container">
+            <h2>{{ text }}</h2>
+            <a href="#">
+                {{ ButtonText }}
+                <i class="fa-solid fa-chevron-right" style="color: #ffffff;"></i>
+            </a>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+@use '../../style/partials/mixins' as *;
+section{
+    text-align: center;
+    background-image: url('../../assets/img/pattern.png');
+    color: white;
+    padding: 2rem 0;
+    .container{
+        @include flex(row,space-around,center);
+        width: 80%;
+        margin: 0 auto;
+        h2{
+            font-size: 2rem;
+        }
+        a{
+            color: white;
+            border: 3px solid white;
+            padding: 10px 20px;
+            i{
+                margin-left: 5px;
+            }
+        }
+    }
+}
+</style>
