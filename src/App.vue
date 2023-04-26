909e2193b35c79ce0154ded5cd1f51d662277fef
Angelop26 <angeloprospero26@gmail.com>
2023-04-26 15:23:24 +0200

    macrocomponents created

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <script>
+import AppFooterVue from './components/AppFooter.vue';
+import AppHeaderVue from './components/AppHeader.vue';
+import AppMainVue from './components/AppMain.vue';
+import {store} from './store';
 export default{
-  
+  components:{
+    AppHeaderVue,
+    AppMainVue,
+    AppFooterVue
+  },
+  data(){
+    return{
+      store
+    }
+  }
 }
 </script>
 
 <template>
-
+<AppHeaderVue />
+<AppMainVue />
+<AppFooterVue />
 </template>
 
 <style lang="scss">
